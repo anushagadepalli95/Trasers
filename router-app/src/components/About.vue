af352ea0d7416c29bd0f5860b75ca07310d29923
anusha.gadepalli <anusha.gadepalli@trianz.com>
2019-02-05 17:52:03 +0530

    first commit

---
+++ router-app/src/components/About.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="login">
+     <!-- <h1>Your IP is {{ ip }}</h1>  -->
+      <div class="row">
+            <Child title="This is my title"></Child>
+             <Child :parentData="{name: 'Noelia O'}"></Child>
+             <Child :parentData="myData"></Child>
+                 <Child :stringProp="stringMessage"></Child>
+        </div>
+
+ 
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Vuetable from 'vuetable-2'
+
+import FieldsDef from "./../FieldsDef.js";
+import Child from "./Hello.vue";
+
+export default {
+  name: 'About',
+ 
+
+   components: {
+    Vuetable   
+  },
+
+  data(){
+    return {
+      name:'Lucas'
+    }
+
+  },
+ components: {
+    Child
+  },
+
+  // mounted() {
+    
+            // axios({ method: "GET", "url": "https://vuetable.ratiw.net/api/users" }).then(result => {
+            //     this.ip = result.data.data;
+            //     console.log(this.ip);
+
+            // }, error => {
+            //     console.error(error);
+            // });
+        // },
+        methods:{
+          sendData() {
+                axios({ method: "POST", "url": "https://httpbin.org/post", "data": this.input, "headers": { "content-type": "application/json" } }).then(result => {
+                    this.response = result.data;
+                    console.log(this.input);
+                }, error => {
+                    console.error(error);
+                });
+            }
+        }
+}
+</script>
+ 
---
+++ router-app/src/components/Hello.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    
+ <h2>My deepest, darkest question: {{name}}</h2>
+<input v-model="name"/>
+
+  </div>
+
+</template>
+
+<script>
+import Vuetable from 'vuetable-2';
+import axios from "axios";
+export default {
+ name: 'Child',
+  // To use props, they must be declared
+  props: {
+    parentData: Object,
+    stringProp: String,
+    title: String
+  },
+ data(){
+    return {
+      name:'Lucas',
+      ip: ''
+    }
+
+  },
+  mounted() {
+    
+            axios({ method: "GET", "url": "https://vuetable.ratiw.net/api/users" }).then(result => {
+                this.ip = result.data.data;
+                console.log(this.ip);
+
+            }, error => {
+                console.error(error);
+            });
+        }
+ }
+
+  
+
+</script>
